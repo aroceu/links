ac79be0754f6ba2983167f088d72bad21bd36813
Arrow Sparrow <dev@aroceu.cloud>
2024-11-20 15:15:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -163,52 +163,53 @@ body{
     
     <div id="sidebar">
         <ul>
-            <li><b><a href="main.php" target="iframe">Home</a></b> | <a href="about.php" target="iframe">About</a></li>
-   <li><a href="graphics.php" target="iframe">Graphic Design</a></li>
-<li><a href="web.php" target="iframe">Web Design, Code, Hosting, and Development Resources</a><ul>
-    <li><a href="websocial.php" target="iframe">Social media/CMS resources</a></li>
-    <li><a href="webcommunity.php" target="iframe">Communities and others' lists of webdev resources</a></li>
-        <li><font color="red"><b>NEW!</b></font> <a href="artists.php" target="iframe">Artist personal website resources masterlist</a></li>
-
+            <li><b><a href="main.php">Home</a></b> | <a href="about.php">About</a></li>
+   <li><a href="graphics.php">Graphic Design</a></li>
+<li><a href="web.php">Web Design, Code, Hosting, and Development Resources</a><ul>
+    <li><a href="websocial.php">Social media/CMS resources</a></li>
+    <li><a href="webcommunity.php">Communities and others' lists of webdev resources</a></li>
+    <li><a href="artists.php">Artist personal website resources masterlist</a>
+    	<ul><li><font color="red"><b>NEW!</b></font> <a href="artsites.php">List of independent art websites</a></li>
+    	</ul></li>
 	</ul></li>
-<li><a href="fandom.php" target="iframe">Fandom, Fanfiction, and Fanworks</a><ul>
-    <li><a href="1d.php" target="iframe">1D</a></li>
-    <li><a href="bl.php" target="iframe">BL masterlists</a></li>
-    <li><a href="communitytv.php" target="iframe">Community</a></li>
-        <li><a href="hp.php" target="iframe">HP</a></li>
-        <li><a href="kpop.php" target="iframe">Kpop</a></li>
-        <li><a href="lost.php" target="iframe">Lost</a></li>
-        <li><a href="lotr.php" target="iframe">LOTR</a></li>
-		<li><a href="mdzs.php" target="iframe">MDZS</a></li>
-		<li><a href="moffat.php" target="iframe">Moffat Critique</a></li>
-		<li><a href="patd.php" target="iframe">PATD</a></li>
-		<li><a href="pentagon.php" target="iframe">Pentagon</a></li>
-		<li><a href="pkmn.php" target="iframe">Pok&eacute;mon</a></li>
-		<li><a href="psych.php" target="iframe">Psych</a></li>
-		<li><a href="splat.php" target="iframe">Splatoon</a></li>
-		<li><a href="suju.php" target="iframe">Super Junior</a></li>
-		<li><a href="thaibl.php" target="iframe">Thai BL</a></li>
-		<li><a href="tsn.php" target="iframe">TSN</a></li>
+<li><a href="fandom.php">Fandom, Fanfiction, and Fanworks</a><ul>
+    <li><a href="1d.php">1D</a></li>
+    <li><a href="bl.php">BL masterlists</a></li>
+    <li><a href="communitytv.php">Community</a></li>
+        <li><a href="hp.php">HP</a></li>
+        <li><a href="kpop.php">Kpop</a></li>
+        <li><a href="lost.php">Lost</a></li>
+        <li><a href="lotr.php">LOTR</a></li>
+		<li><a href="mdzs.php">MDZS</a></li>
+		<li><a href="moffat.php">Moffat Critique</a></li>
+		<li><a href="patd.php">PATD</a></li>
+		<li><a href="pentagon.php">Pentagon</a></li>
+		<li><a href="pkmn.php">Pok&eacute;mon</a></li>
+		<li><a href="psych.php">Psych</a></li>
+		<li><a href="splat.php">Splatoon</a></li>
+		<li><a href="suju.php">Super Junior</a></li>
+		<li><a href="thaibl.php">Thai BL</a></li>
+		<li><a href="tsn.php">TSN</a></li>
 		</ul></li>
-	<li><a href="oldfansites.php" target="iframe">Old personal fanfiction sites</a></li>
-		<li><a href="libraries.php" target="iframe">Libraries</a></li>
-<li><a href="writing.php" target="iframe">Writing</a>
-    <ul><li><a href="genre.php" target="iframe">Genre Commentary</a></li></ul></li>
+	<li><a href="oldfansites.php">Old personal fanfiction sites</a></li>
+<li><a href="writing.php">Writing</a>
+    <ul><li><a href="genre.php">Genre Commentary</a></li></ul></li>
+<li><a href="libraries.php">Libraries & Archives</a></li>
 <li>Reading<ul>
-	<li><a href="reading.php" target="iframe">Social Commentary</a></li>
-	<li><a href="readinglist.php" target="iframe">Reading lists (books & articles)</a></li>
-	<li><a href="wikipedia.php" target="iframe">Wikipedia Pages</a></li>
+	<li><a href="reading.php">Social Commentary</a></li>
+	<li><a href="readinglist.php">Reading lists (books & articles)</a></li>
+	<li><a href="wikipedia.php">Wikipedia Pages</a></li>
 	</ul></li>
-<li><a href="tools.php" target="iframe">Online Tools</a><ul>
-	<li><a href="media.php" target="iframe">Media Resources</a></li>
+<li><a href="tools.php">Online Tools</a><ul>
+	<li><a href="media.php">Media Resources</a></li>
 	</ul></li>
-<li><a href="games.php" target="iframe">Games</a></li>
-<li><a href="irl.php" target="iframe">IRL</a>
-    <ul><li><a href="recipes.php" target="iframe">Recipes</a></li></ul>
-<li><a href="personality.php" target="iframe">Personality Systems</a></li>
-<!--<li><a href="arrrgh.php" target="iframe">Try this link!</a></li> -->
-<li><a href="palestine.php" target="iframe">Palestine</a></li>
-<li><a href="links.php" target="iframe">More links (of links)</a></li>
+<li><a href="games.php">Games</a></li>
+<li><a href="irl.php">IRL</a>
+    <ul><li><a href="recipes.php">Recipes</a></li></ul>
+<li><a href="personality.php">Personality Systems</a></li>
+<!--<li><a href="arrrgh.php">Try this link!</a></li> -->
+<li><a href="palestine.php">Palestine</a></li>
+<li><a href="links.php">More links (of links)</a></li>
     
     
 </ul>
