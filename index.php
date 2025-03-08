e500fe3964c9dd257eaae22b46874834984a21af
arrow sparrow <dev@aroceu.cloud>
2025-03-08 03:58:39 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="A directory of assorted links."/>
 	<link rel="shortcut icon" type="image/x-icon" href="https://kingdra.net/links/link.ico" />
 
 
